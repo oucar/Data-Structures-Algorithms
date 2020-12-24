cbebac56cf96118613384d18349a881b5c15e90b
Onur Ucar <ucarthegreat@gmail.com>
2020-12-23 23:38:48 -0500

    power completed

--- 2- Recursion/power.cpp
+++ 2- Recursion/power.cpp
@@ -1,21 +1,31 @@
-/* Challenge: Power of Given Number */
+/* Challenge: Power of Given Number 
+	multiplying m, n times
+
+	2^5 = 2*2*2*2*2 
+	power(m,n) = m*m*m*m.....*(n-1)*m
+	power(m,n) = power(m,n-1)*m
+*/
 #include <iostream>
 
 //Function declarations
-int power(int nGiven, int mGiven);
+int power(int mGiven, int nGiven);
 
-int power(int nGiven, int mGiven) {
+int power(int mGiven, int nGiven) {
 
-	if (mGiven == 0) return 1;
-	else return power(nGiven, mGiven - 1) * nGiven; //power(2,0)*2 = 2, power(2,1)*2 = 2*2..
+	if (nGiven == 0) return 1;
+
+	else if (nGiven % 2 == 0) return power((mGiven * mGiven), nGiven / 2); // 2^8 = (2^2)^4
+
+	else return mGiven*(power(mGiven * mGiven, (nGiven - 1) / 2)); // 2^9 = (2^2)^(2^2)*2
+
 }//end power()
 
 
 int main() {
-	int n = 5;
-	int m = 3; // 5^3 = 125
+	int m = 5;
+	int n = 3; // 5^3 = 125
 
-	std::cout << power(n, m);
+	std::cout << power(m,n);
 
 	return 0;
 }
