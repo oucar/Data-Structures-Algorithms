0ae9498680f79cf8dac6dec9281f6c1c6429791d
Onur Ucar <onurucar@iu.edu>
2021-01-04 14:22:39 -0500

    Menu driven program

--- 4- Array ADT/Menu Driven Program/array.h
+++ 4- Array ADT/Menu Driven Program/array.h
@@ -1,71 +1,85 @@
+#ifndef UNTITLED_ARRAY_H
+#define UNTITLED_ARRAY_H
+
 class Array {
 
 private:
-	int* A;
-	int size;
-	int length;
+    int* A;
+    int size;
+    int length;
 
-	void swap(int* x, int* y);							// Swap two numbers (Helper function, that's why it's private.)
+    void swap(int* x, int* y);							// Swap two numbers (Helper function, that's why it's private.)
 
 public:
 
-	Array() {											// Constructor without parameter
-		size = 10;
-		length = 0;
-		A = new int[size];
-	}
+    Array() {											// Constructor without parameter
+        size = 10;
+        length = 0;
+        A = new int[size];
+    }
 
-	Array(int sizeGiven) {								// Constructor with a given size
-		size = sizeGiven;
-		length = 0;
-		A = new int[size];
-	}
+    Array(int sizeGiven) {								// Constructor with a given size
+        size = sizeGiven;
+        length = 0;
+        A = new int[size];
+    }
 
-	Array(int arrGiven[], int sizeGiven) {			   // Constructor with a given size and array 
-		size = sizeGiven;
-		length = sizeof(arrGiven) / sizeof(int);
-		A = new int[size];
+    Array(int arrGiven[], int sizeGiven) {			   // Constructor with a given size and array
+        size = sizeGiven;
+        length = sizeof(arrGiven) / sizeof(int);
+        A = new int[size];
 
-		for (int i = 0; i < size; i++)
-			A[i] = arrGiven[i];
-	}
+        for (int i = 0; i < size; i++)
+            A[i] = arrGiven[i];
+    }
 
-	~Array() {											// Destructor
-		delete[]A;
-	}
+    ~Array() {											// Destructor
+        delete[]A;
+    }
 
-	void display();										// Display
-	int get(int index);									// Get the value of a given index
-	int set(int index, int x);							// Set an index to a given element
+    void set(int index, int x);							// Set an index to a given element
+    int get(int index);									// Get the value of a given index
+    void display();										// Display
 
-	int getSize();										// Get the size
-	void setSize(int x);								// Set the size
-	int getLength();									// Get the length
-	void setLength(int x);								// Set the length
+    int getSize();										// Get the size
+    void setSize(int x);								// Set the size
+    int getLength();									// Get the length
+    void setLength(int x);								// Set the length
 
-	void append(int x);									// Insert an element at the end of the list.
-	void insert(int index, int x);						// Insert an element to a given index
+    void append(int x);									// Insert an element at the end of the list.
+    void insert(int index, int x);						// Insert an element to a given index                   --> Elements after index will be shifted.
+    void remove(int index);                             //@TODO Delete an element from the given index.
 
-	int linearSearch(int key);							// Linear search
-	int binarySearch(int key);							// Binary search
-	int recursiveBinarySearch(int key);					// **Recursive** binary search
+    int linearSearch(int key);							            // Linear search
+    int binarySearch(int key);							            // Binary search
+    int recursiveBinarySearch(int key, int low, int high);          // **Recursive** binary search
 
-	int max();											// Maximum element
-	int min();											// Minimum element
-	int sum();											// Sum of all the elements
-	int average();										// Average of all the elements
+    int max();											// Maximum element
+    int min();											// Minimum element
+    int sum();											// Sum of all the elements
+    float average();										// Average of all the elements
 
-	void reverse();										// Reverse the given array
-	void reverse2();									// Another way to reverse an array
-		
-	void insertSorted(int x);							// Insert an element to a sorted array
-	void sortAndInsert(int x);							// Sort the array and insert the given array
+    void reverse();										// Reverse the given array
+    void reverse2();									// Another way to reverse an array
 
-	Array* mergeOperation(Array* arrGiven);				// Merges two array and returns it as an array
-	Array* unionOperation(Array* arrGiven);				// Finds the union of two array and returns it as an array
-	Array* intersectionOperation(Array* arrGiven);		// Finds the intersection of two array and returns it as an array
-	Array* differenceOperation(Array* arrGiven);		// Finds the difference of two array and returns it as an array
+    void insertSorted(int x);							// Insert an element to a sorted array
+    void sortAndInsert(int x);							// Sort the array and insert the given array
 
-	
-														//TODO 1 Create as many instances as you want and make them accesible through the menu.
+    Array* mergeOperation(Array* arrGiven);				// Merges two array and returns it as an array
+    Array* unionOperation(Array* arrGiven);				// Finds the union of two array and returns it as an array
+    Array* intersectionOperation(Array* arrGiven);		// Finds the intersection of two array and returns it as an array
+    Array* differenceOperation(Array* arrGiven);		// Finds the difference of two array and returns it as an array
+
+
+    //TODO 1 Create as many instances as you want and make them accessible through the menu.
 };
+
+
+
+
+
+
+
+
+
+#endif
