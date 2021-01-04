87c1ceb265a65e8fbe52a9b536286201065b71ee
oucar <hello@ucaronur.com>
2021-01-04 14:22:39 -0500

    Menu driven program

--- 4- Array ADT/Menu Driven Program/array.cpp
+++ 4- Array ADT/Menu Driven Program/array.cpp
@@ -1,2 +1,170 @@
 #include "array.h"
+#include <iostream>
+
+//                                  SET, GET, DISPLAY, SWAP                               //
+
+void Array::set(int index,
+                int x) {                                                      // Set (not required, same as insert)
+    A[index] = x;
+};
+
+int Array::get(int index) {                                                              // Get
+    return A[index];
+};
+
+void Array::display() {                                                                  // Display
+    for (int i = 0; i < size; i++)
+        std::cout << A[i] << " ";
+    std::cout << std::endl;
+}
+
+
+void Array::swap(int *x, int *y) {                                                        // Swap
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+//                          GETTER AND SETTER -> SIZE AND LENGTH                        //
+
+int Array::getSize() {                                                                  // Get size
+    return size;
+}
+
+void Array::setSize(int x) {                                                            // Set size
+    size = x;
+}
+
+int Array::getLength() {                                                                 // Get length
+    return length;
+}
+
+void Array::setLength(int x) {                                                           // Set length
+    length = x;
+};
+
+//                                 APPEND, INSERT, REMOVE                                //
+
+void Array::append(int x) {                                                              // Append
+
+    if (size > length) {
+        A[length] = x;
+        length++;
+    }
+
+}
+
+void Array::insert(int index, int x) {                                                   // Index
+
+    if (index >= 0 && index <= length) {                                                 // will be shifted
+        for (int i = length; i > index; i--)
+            A[i] = A[i - 1];
+        A[index] = x;
+        length++;
+    }
+
+}
+
+void Array::remove(int index){                                                          // Remove
+
+    if(index >= 0 && index <= length){
+        for(int i = index; i<length; i++)                                               // will be shifted backwards
+            A[i] = A[i+1];
+
+        length--;
+    }
+}
+
+//                               LINEAR AND BINARY SEARCH                                //
+
+int Array::linearSearch(int key) {                                                       // Linear search       O(n)
+
+    for (int currNum = 0; currNum < size; currNum++) {
+        if (A[currNum] == key)
+            //swap(&A[currNum], &A[currNum]);                                            // Transposition -> Can be enabled
+            return currNum;
+    }
+    return -1;
+
+};
+
+int
+Array::binarySearch(int key) {                                                       // Binary search        O(log(n))
+
+    int low, mid, high;
+    low = 0;
+    high = length - 1;
+
+    while (low <= high) {
+
+        mid = (high + low) / 2;
+
+        if (key == A[mid])                                                                // Check left
+            return mid;
+
+        else if (key < A[mid])                                                           // Check right
+            high = mid - 1;
+
+        else low = mid + 1;
+    }
+
+    return -1;
+};
+
+int Array::recursiveBinarySearch(int key, int low, int high) {                  // **Recursive** binary search
+
+    int mid = 0;
+    high = length - 1;
+
+    if (low <= high) {
+        mid = (low + high) / 2;
+
+        if (key == A[mid])
+            return mid;
+
+        else if (key < A[mid])
+            return recursiveBinarySearch(key, low, mid - 1);
+
+        else return recursiveBinarySearch(key, mid + 1,  high);
+    }//end if
+};
+
+//                               MAX, MIN, SUM, AVG                               //
+
+
+int Array::max(){
+
+    int max = A[0];
+    for(int i = 0; i < length; i++)
+        if(A[i] > max) {
+            max = A[i];
+        }
+    return max;
+};
+
+int Array::min(){
+
+    int min = A[0];
+    for(int i = 0; i< length; i++)
+        if(A[i] < min ) {
+            min = A[i];
+        }
+    return  min;
+
+};
+
+int Array::sum(){
+
+    int sum = 0;
+
+    for(int i = 0; i<length; i++)
+        sum += A[i];
+
+    return sum;
+};
+
+float Array::average(){
+
+    return sum()/length;
+};
 
--- 4- Array ADT/Menu Driven Program/main.cpp
+++ 4- Array ADT/Menu Driven Program/main.cpp
@@ -1,20 +1,42 @@
 /* Array ADT */
-#include <iostream>
 #include "array.h"
+#include <iostream>
+
 
 int main(){
 
-	
-	Array* arr;										//Array *arr = new Array[10];	// Creates 10 array objects
+//    Array* arr;										//Array *arr = new Array[10];	// Creates 10 array objects
+//
+//    std::cout << "size: ";
+//    int sz;
+//    std::cin >> sz;
+//
+//    arr = new Array(sz);
 
-	std::cout << "size: ";
-	int sz;
-	std::cin >> sz;
+    Array arr(10);
 
-	arr = new Array(sz);
-	
+    arr.append(1);
+    arr.append(2);
+    arr.append(3);
+    arr.append(4);
+    arr.append(5);
+    arr.append(6);
+    arr.append(7);
+    arr.append(8);
+    arr.append(9);
+    arr.insert(9,10);
 
-	
 
-	return 0;
+
+
+    arr.display();
+
+    //arr.display();
+
+    std::cout << arr.getSize() <<  " " << arr.getLength() << " " << arr.max() ;
+
+
+
+
+    return 0;
 }
