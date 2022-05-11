704bb65cc84958c466efea8cb47edde6e1ea4baf
oucar <hello@ucaronur.com>
2022-05-11 14:31:35 -0400

    simplified sumzero.js

--- JS, 1- Problem Solving Patterns/sumZero.js
+++ JS, 1- Problem Solving Patterns/sumZero.js
@@ -1,21 +1,17 @@
+// MULTIPLE POINTERS PATTERN
+
 function sumZero(arr){
     let left = 0;
     let right = arr.length - 1;
 
     while(left < right){
-
         let sum = arr[left] + arr[right];
 
-        if(sum === 0){
-            return [arr[left], arr[right]];
-        }else if(sum > 0){
-            right--;
-        }else{
-            left++;
-        } // end if
+        if(sum === 0) return [arr[left], arr[right]];
+        else if(sum > 0) right--;
+        else left++;
+    }
 
-    } // end while
-
-} // end sumZero(arr)
+}
 
 console.log(sumZero([-4, -3, -2, -1, 0, 1, ,2 ,3, 10]));
