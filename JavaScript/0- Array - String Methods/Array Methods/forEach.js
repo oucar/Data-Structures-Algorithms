353a10024efb7d21d1a3cc5c53a0fefeb6ba27e6
oucar <hello@ucaronur.com>
2022-10-16 14:05:40 -0400

    for each and map

---
+++ JavaScript/0- Array - String Methods/Array Methods/forEach.js
@@ -0,0 +1,36 @@
+// executes a given function for every element in an array
+
+const numbers = [1, 2, 3, 4, 5];
+// (item, index)
+numbers.forEach((item, index) => {
+    console.log(`arr[${index}] = ${item}`);
+});
+
+console.log('----');
+
+
+// (item, index, arr)
+numbers.forEach((item, index, arr) => {
+    console.log(`arr[${arr[index]-1}] = ${item}`);
+})
+
+
+console.log('----');
+
+// calculating sum
+let sum = 0;
+numbers.forEach((item) => {
+    sum += item;
+})
+console.log(sum);
+
+
+console.log('----');
+
+
+// how many times a letter appears in an array (extra-object counter method)
+const letters = ['a', 'b', 'c', 'a', 'o', 'd', 'e'];
+let obj = {};
+for (let val of letters) obj[val] = (obj[val] || 0) + 1;
+
+console.log(obj);
---
+++ JavaScript/0- Array - String Methods/Array Methods/map.js
@@ -0,0 +1,45 @@
+// executes a function and returns a new array
+
+const numbers = [1, 2, 3, 4, 5];
+
+// val, index, arr
+// return the double
+const doubleArray = numbers.map(val => val * 2);
+console.log(doubleArray);
+
+// return value times index
+const valueTimesIndexArray = numbers.map((val, index) => {
+    return val * index;
+})
+console.log(valueTimesIndexArray);
+
+// example with array of objects
+// calculate the total worth of all items
+const inv = [
+    {
+        name: 'laptop',
+        price: 100,
+        count: 5
+    },
+    {
+        name: 'steam deck',
+        price: 500,
+        count: 1
+    },
+    {
+        name: 'iphone',
+        price: 1000,
+        count: 500
+    },
+    {
+        name: 'nintendo switch',
+        price: 300,
+        count: 20000
+    }
+]
+
+const totalProductsValue = inv.map(item => {
+    // item is an object
+    return {name: item.name, totalValue: item.price * item.count};
+})
+console.log(totalProductsValue)
