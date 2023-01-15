7a110f6d7e04ae8f524dd3aaf2b3e52779b6f859
oucar <hello@ucaronur.com>
2023-01-15 02:47:27 -0500

    binary search

---
+++ JavaScript/3- Searching Algorithms/binarySearch.js
@@ -0,0 +1,32 @@
+// DIVIDE AND CONQUER!
+
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (start <= end) {
+    if (arr[middle] === val) return middle;
+    // value is on the left
+    else if (val < arr[middle]) {
+      end = middle - 1;
+      middle = Math.floor((start + end) / 2);
+    }
+
+    // value is on the right
+    else if (val > arr[middle]) {
+      start = middle + 1;
+      middle = Math.floor((start + end) / 2);
+    }
+  }
+
+  return -1;
+}
+
+let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+console.log(binarySearch(arr, 20));
+console.log(binarySearch(arr, 100));
+console.log(binarySearch(arr, 10));
+console.log(binarySearch(arr, 31));
+console.log(binarySearch(arr, 50));
