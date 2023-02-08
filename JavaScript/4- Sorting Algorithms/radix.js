0068855580e711dfba82d5a6eae346d8c5f5f1b0
oucar <hello@ucaronur.com>
2023-02-08 00:05:13 -0500

    Done with the radix sort

--- JavaScript/4- Sorting Algorithms/radix.js
+++ JavaScript/4- Sorting Algorithms/radix.js
@@ -21,3 +21,18 @@ function mostDigits(nums) {
     maxDigits = Math.max(maxDigits, digitCount(nums[i]));
   }
 }
+
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      let digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
+
+radixSort([23, 345, 5467, 12, 2345, 9852]);
