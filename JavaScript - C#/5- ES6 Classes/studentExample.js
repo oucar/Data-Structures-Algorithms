af380888e5fea5e0415fdb74dbb766704f99f0b3
oucar <hello@ucaronur.com>
2023-02-11 17:04:21 -0500

    added an es6 class example

---
+++ JavaScript - C#/5- ES6 Classes/studentExample.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = year;
+    }
+
+    fullName(){
+        return `Your full name is ${this.firstName} ${this.lastName}.`
+    }
+}
+
+
+let studentOnur = new Student("Onur", "Ucar", 4);
+studentOnur.fullName();
