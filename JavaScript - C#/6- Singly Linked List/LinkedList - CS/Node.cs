e22cabb948acf7accbb39296b3422ecd9a504f64
oucar <hello@ucaronur.com>
2023-02-28 21:31:18 -0500

    .net code refactoring

--- JavaScript - C#/6- Singly Linked List/LinkedList - CS/Node.cs
+++ JavaScript - C#/6- Singly Linked List/LinkedList - CS/Node.cs
@@ -3,23 +3,17 @@ namespace LinkedList___CS
 {
 	public class Node
 	{
-        #region Fields
         private object _data;
 		private Node _next;
-        #endregion
 
-        #region Constructor
         public Node(object _data, Node _next)
 		{
 			this._data = _data;
 			this._next = _next;
 		}
-        #endregion
 
-        #region Public Getter and Setters
         public object Data { get; set; }
 		public Node Next { get; set; }
-        #endregion
 
     }
 }
