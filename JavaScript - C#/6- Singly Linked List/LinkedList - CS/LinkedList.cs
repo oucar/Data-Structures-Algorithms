fce85db69a14634a0f13f966244c7dc464149322
oucar <hello@ucaronur.com>
2023-02-28 21:27:51 -0500

    .net node implementation

---
+++ JavaScript - C#/6- Singly Linked List/LinkedList - CS/LinkedList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LinkedList___CS
+{
+	public class LinkedList
+	{
+		public LinkedList()
+		{
+		}
+	}
+}
+
---
+++ JavaScript - C#/6- Singly Linked List/LinkedList - CS/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LinkedList___CS
+{
+	public class Node
+	{
+        #region Fields
+        private object _data;
+		private Node _next;
+        #endregion
+
+        #region Constructor
+        public Node(object _data, Node _next)
+		{
+			this._data = _data;
+			this._next = _next;
+		}
+        #endregion
+
+        #region Public Getter and Setters
+        public object Data { get; set; }
+		public Node Next { get; set; }
+        #endregion
+
+    }
+}
+
