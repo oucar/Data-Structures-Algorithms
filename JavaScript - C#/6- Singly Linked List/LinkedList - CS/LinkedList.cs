06fe8b44f76d5c4a3a7fe05ece8be61f732915a9
oucar <hello@ucaronur.com>
2023-03-05 23:25:36 -0500

    beautify

--- JavaScript - C#/6- Singly Linked List/LinkedList - CS/LinkedList.cs
+++ JavaScript - C#/6- Singly Linked List/LinkedList - CS/LinkedList.cs
@@ -4,69 +4,75 @@ using System.Diagnostics.Metrics;
 
 namespace LinkedList___CS
 {
-	public class LinkedList
-	{
-		private Node _head;
-		private Node _tail;
-		private int _size;
+    public class LinkedList
+    {
+        private Node _head;
+        private Node _tail;
+        private int _size;
 
-		public LinkedList()
-		{
-			this._head = null;
-			this._tail = null;
-			this._size = 0;
-		}
+        public LinkedList()
+        {
+            this._head = null;
+            this._tail = null;
+            this._size = 0;
+        }
 
-		public bool isEmpty()
-		{
-			return this._head == null ? true : false;
-		}
+        public bool isEmpty()
+        {
+            return this._head == null ? true : false;
+        }
 
-		public int Count()
-		{
-			return this._size;
-		}
+        public int Count()
+        {
+            return this._size;
+        }
 
-		/// <summary>
-		/// Helper Function. Lets you print the linked list.
-		/// </summary>
-		public void Print()
-		{
-			Node current = this._head;
-			while(current != null)
-			{
-				Console.Write($"{current.Data} --> ");
-				current = current.Next;
-			}
-			Console.WriteLine("null");
-			Console.WriteLine($"Size: {Count()}");
-		}
+        /// <summary>
+        /// Helper Function. Lets you print the linked list.
+        /// </summary>
+        public void Print()
+        {
+            Node current = this._head;
+            while (current != null)
+            {
+                Console.Write($"{current.Data} --> ");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+            Console.WriteLine($"Size: {Count()}");
+        }
 
-		/// <summary>
-		/// Inserts a new node at the end of the linked list.
-		/// </summary>
-		/// <param name="data">Value that we will be inserting</param>
-		/// <returns></returns>
+        /// <summary>
+        /// Inserts a new node at the end of the linked list.
+        /// </summary>
+        /// <param name="data">Value that we will be inserting</param>
+        /// <returns></returns>
         public Node Push(int data)
-		{
-			// if (index < 0 || index > this._size + 1) throw new IndexOutOfRangeException($"Index can't be smaller than 0 or larger than (this._size + 1). Given index: {index}");
+        {
+            // if (index < 0 || index > this._size + 1) throw new IndexOutOfRangeException($"Index can't be smaller than 0 or larger than (this._size + 1). Given index: {index}");
 
-			Node node = new Node(data);
-			if (isEmpty())
-			{
-				this._head = node;
-				this._tail = node;
-			} else
-			{
-				this._tail.Next = node;
-				this._tail = node;
-			}
+            Node node = new Node(data);
+            if (isEmpty())
+            {
+                this._head = node;
+                this._tail = node;
+            }
+            else
+            {
+                this._tail.Next = node;
+                this._tail = node;
+            }
 
             node.Data = data;
             this._size++;
 
-			return this._tail;
-		}
-	}
+            return this._tail;
+        }
+
+        public Node pop()
+        {
+            if (isEmpty()) return null;
+        }
+    }
 }
 
