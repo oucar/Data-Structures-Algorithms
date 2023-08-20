9ff55b1f213a9e22b43e6206bec58ec2b7b332b5
oucar <hello@ucaronur.com>
2023-08-20 16:35:08 +0300

    is arraySubset implementation done

--- Common-Sense-Guide-to-DSA/8- Hash Tables/ArraySubset.cs
+++ Common-Sense-Guide-to-DSA/8- Hash Tables/ArraySubset.cs
@@ -1,11 +1,56 @@
+using System.Collections;
+
 class Program
 {
 
+    static bool IsSubset(int[] arr1, int[] arr2)
+    {
+        int[] largerArray;
+        int[] smallerArray;
+        var hashTable = new Hashtable();
+
+        // Determine which array is smaller
+        if(arr1.Length > arr2.Length)
+        {
+            largerArray = arr1;
+            smallerArray = arr2;
+        }
+        else
+        {
+            largerArray = arr2;
+            smallerArray = arr1;
+        }
+
+        // Store all elements from largerArray inside the HashTable
+        foreach(int item in largerArray)
+        {
+            hashTable.Add(item, true);
+        }
+
+        // Iterate thru each item in smallerArray and return false if that's the case
+        foreach(int item in smallerArray)
+        {
+            if (hashTable[item] == null) return false;
+        }
+
+        return true;
+
+    }
 
     static void Main(string[] args)
     {
+        int[] numbers = { 61, 30, 91, 11, 54, 38, 72 };
+        int[] arr1 = { 91, 54, 72 };
+        int[] arr2 = { 61, 30, 91, 11, 54, 38, 72 };
+        int[] arr3 = { 91, 30 };
+        int[] arr4 = { };
+        int[] arr5 = { 100, 200 };
 
-
+        Console.WriteLine(IsSubset(numbers, arr1));
+        Console.WriteLine(IsSubset(numbers, arr2));
+        Console.WriteLine(IsSubset(numbers, arr3));
+        Console.WriteLine(IsSubset(numbers, arr4));
+        Console.WriteLine(IsSubset(numbers, arr5));
 
     }
 }
