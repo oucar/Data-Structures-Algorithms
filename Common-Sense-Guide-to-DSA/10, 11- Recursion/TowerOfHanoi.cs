6da6a424f300bd82c9aade7b2c6689ecd9375bc8
oucar <hello@ucaronur.com>
2023-10-31 23:50:31 -0400

    Added Tower of Hanoi problem to recursion

---
+++ Common-Sense-Guide-to-DSA/10, 11- Recursion/TowerOfHanoi.cs
@@ -0,0 +1,26 @@
+using Playground;
+
+class Program
+{
+
+    // Solution for N discs:
+    // Move (n-1) Discs from A to B using C.
+    // Move 1 Disc from A to C.
+    // Move (n-1) Discs from B to C using A.
+
+    // source, destination and aux 
+    static void towerOfHanoi(int number, char A, char B, char C)
+    {
+        if (number == 0) return;
+
+        towerOfHanoi(number - 1, A, C, B);
+        Console.WriteLine($"Moving a disc from {A} to {B} using {C}");
+        towerOfHanoi(number - 1, B, A, C);
+    }
+
+
+    static void Main(string[] args)
+    {
+        towerOfHanoi(3, 'A', 'C', 'B');
+    }
+}
