4236130c9096ba002708c0527f340e460bb6edaf
Onur Ucar <hello@ucaronur.com>
2024-03-18 12:01:49 -0400

    solved vowels question

--- JavaScript - C#/Extra/vowels/index.js
+++ JavaScript - C#/Extra/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowelsArr = ["a", "e", "i", "o", "u"];
+  let objCounter = {};
+  let counter = 0;
+
+  str = str.toLowerCase();
+
+  for (let val of str) objCounter[val] = (objCounter[val] || 0) + 1;
+
+  for (let val of vowelsArr) if (objCounter[val]) counter++;
+
+  return counter;
+}
 
 module.exports = vowels;
