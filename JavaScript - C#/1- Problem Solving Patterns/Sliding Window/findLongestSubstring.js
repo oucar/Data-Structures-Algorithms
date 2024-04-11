f58fd4555aa4b903e6c6f50f79b2b3132ffcb401
Onur Ucar <hello@ucaronur.com>
2024-04-11 16:51:42 -0400

    Refactor findLongestSubstring function in findLongestSubstring.js

--- JavaScript - C#/1- Problem Solving Patterns/Sliding Window/findLongestSubstring.js
+++ JavaScript - C#/1- Problem Solving Patterns/Sliding Window/findLongestSubstring.js
@@ -1,20 +1,28 @@
-
-// Write a function called findLongestSubstring, which accepts a string and returns 
+// Write a function called findLongestSubstring, which accepts a string and returns
 // the length of the longest substring with all distinct characters.
 function findLongestSubstring(str) {
-    let longest = 0;
-    let seen = {};
-    let start = 0;
-   
-    for (let i = 0; i < str.length; i++) {
-      let char = str[i];
-      if (seen[char]) {
-        start = Math.max(start, seen[char]);
-      }
-      // index - beginning of substring + 1 (to include current in count)
-      longest = Math.max(longest, i - start + 1);
-      // store the index of the next char so as to not double count
-      seen[char] = i + 1;
+  let lastSeenIndex = {};
+  let longestSize = 0;
+  let windowStart = 0;
+
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    let rightChar = str[windowEnd];
+
+    if (rightChar in lastSeenIndex) {
+      windowStart = Math.max(windowStart, lastSeenIndex[rightChar] + 1);
     }
-    return longest;
+
+    lastSeenIndex[rightChar] = windowEnd;
+    longestSize = Math.max(longestSize, windowEnd - windowStart + 1);
   }
+
+  return longestSize;
+}
+
+console.log(findLongestSubstring("")); // 0
+console.log(findLongestSubstring("rithmschool")); // 7
+console.log(findLongestSubstring("thisisawesome")); // 6
+console.log(findLongestSubstring("thecatinthehat")); // 7
+console.log(findLongestSubstring("bbbbbb")); // 1
+console.log(findLongestSubstring("longestsubstring")); // 8
+console.log(findLongestSubstring("thisishowwedoit")); // 6
