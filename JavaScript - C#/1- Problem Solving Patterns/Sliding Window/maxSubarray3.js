1e71bb3895102bce63ec51b5e37e5dcb79858ef6
Onur Ucar <hello@ucaronur.com>
2024-04-12 00:48:21 -0400

    Refactor maxSubarraySum function in maxSubarray3.js

--- JavaScript - C#/1- Problem Solving Patterns/Sliding Window/maxSubarray3.js
+++ JavaScript - C#/1- Problem Solving Patterns/Sliding Window/maxSubarray3.js
@@ -1,36 +1,32 @@
 // Find the max sum subarray of a fixed sized K
 // [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
 
-
 // O(N)
-function maxSubarraySum(arr, num){
-    let maxSum = -Infinity;
-    let tempSum = 0;
+function maxSubarraySum(arr, num) {
+  let maxSum = -Infinity;
+  let tempSum = 0;
 
-    if(arr.length < num) return null;
+  if (arr.length < num) return null;
 
-    for(let i = 0; i < arr.length; i++){
-        // add the first numbers
-        tempSum += arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    // add the first numbers
+    tempSum += arr[i];
 
-        // if num is 3, you have 3 elements in the list.
-        if(i >= num - 1){
-            maxSum = Math.max(tempSum, maxSum);
-            // slide the window! (substract 4, add 7, etc.)
-            // substract the furthest left value.
-            // 2 - (3-1) for the first window
-            tempSum -= arr[i - (num-1)];
-        }
+    // if num is 3, you have 3 elements in the list.
+    if (i >= num - 1) {
+      maxSum = Math.max(tempSum, maxSum);
+      // slide the window! (substract 4, add 7, etc.)
+      // substract the furthest left value.
+      // 2 - (3-1) for the first window
+      tempSum -= arr[i - (num - 1)];
     }
+  }
 
-    return maxSum;
-
+  return maxSum;
 }
 
-
-
-console.log(maxSubarraySum([1,2,5,2,8,1,5], 2));             // 10
-console.log(maxSubarraySum([100,200,300,400], 2));           // 700
-console.log(maxSubarraySum([2,3], 3));                       // null 
-console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2));     // 5 
-console.log(maxSubarraySum([2,3,1], 3));                     // 6  
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+console.log(maxSubarraySum([2, 3], 3)); // null
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)); // 5
+console.log(maxSubarraySum([2, 3, 1], 3)); // 6
---
+++ JavaScript - C#/1- Problem Solving Patterns/Sliding Window/maximumSubarrayKadane.js
@@ -0,0 +1,36 @@
+// Given an integer array nums, find the subarray
+//  with the largest sum, and return its sum.
+
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+// Example 3:
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+// AKA: Kadane's algorithm
+var maxSubArray = function (nums) {
+    let maxSum = nums[0];
+    let currentSum = maxSum;
+
+    // first one is already in currentSum
+    for (let i = 1; i < nums.length; i++) {
+        // which one is bigger - currentSum + current element or is current element negative maybe?
+        currentSum = Math.max(nums[i] + currentSum, nums[i]);
+        maxSum = Math.max(currentSum, maxSum);
+    }
+
+    return maxSum;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+console.log(maxSubArray([1])); // 1
+console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
